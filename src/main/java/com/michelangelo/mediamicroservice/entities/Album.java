841a4dcff77a739895dd8d29ef97e72e76318df6
Morten Sandgrav <morten@sandgrav.se>
2024-09-03 16:43:15 +0200

    Added NumberOnAlbum and Album class and attached them to Media

--- src/main/java/com/michelangelo/mediamicroservice/entities/Album.java
+++ src/main/java/com/michelangelo/mediamicroservice/entities/Album.java
@@ -2,16 +2,12 @@ package com.michelangelo.mediamicroservice.entities;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
 public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @OneToMany(targetEntity = NumberOnAlbum.class)
-    List<NumberOnAlbum> songs;
 
     public Album() {
     }
@@ -22,14 +18,6 @@ public class Album {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public List<NumberOnAlbum> getSongs() {
-        return songs;
-    }
-
-    public void setSongs(List<NumberOnAlbum> songs) {
-        this.songs = songs;
     }
 
     public void setId(Long id) {
