6605603adf800ce35e4bc5045c3b7ca471c09a0f
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2024-09-03 10:40:54 +0200

    Initial commit

---
+++ src/test/java/com/michelangelo/mediamicroservice/MediaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelangelo.mediamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
