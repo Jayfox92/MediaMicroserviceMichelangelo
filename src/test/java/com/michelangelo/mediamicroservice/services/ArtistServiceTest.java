7bb79523776606d7847d68a89ea90e21faf6d893
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2024-09-05 13:12:09 +0200

    1

---
+++ src/test/java/com/michelangelo/mediamicroservice/services/ArtistServiceTest.java
@@ -0,0 +1,22 @@
+package com.michelangelo.mediamicroservice.services;
+
+import com.michelangelo.mediamicroservice.entities.Artist;
+import com.michelangelo.mediamicroservice.entities.Media;
+import com.michelangelo.mediamicroservice.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtistServiceTest {
+
+    /*@Test
+    public void shouldReturn
+
+    public List<Media> getMediaByArtist(long id) {
+        Artist artist = artistRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Artist","id",id));
+        return artist.getCreatedMedia();
+    }*/
+
+}
