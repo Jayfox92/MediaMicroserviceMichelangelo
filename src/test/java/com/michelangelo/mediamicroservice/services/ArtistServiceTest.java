b9be6bdc6a60c59d50f1b42b28827b982cf52e72
EvelinaDaun <evelina.daun@hotmail.com>
2024-09-09 13:59:04 +0200

    Test updates for artistService

--- src/test/java/com/michelangelo/mediamicroservice/services/ArtistServiceTest.java
+++ src/test/java/com/michelangelo/mediamicroservice/services/ArtistServiceTest.java
@@ -21,23 +21,19 @@ import static org.mockito.Mockito.when;
 class ArtistServiceTest {
 
     private ArtistService artistService;
-
-    // TODO lägga ihop artistservice & mock till en istället för två
-    private ArtistService artistService1;
     private ArtistRepository artistRepositoryMock = mock(ArtistRepository.class);
+    private Artist artist;
 
     @BeforeEach
     void setUp(){
-        artistService1 = new ArtistService(artistRepositoryMock);
+        artistService = new ArtistService(artistRepositoryMock);
+        artist = new Artist();
     }
 
     @Test
     public void shouldNotThrowIfArtistExistsAndReturnEmptyResultlist(){
-        ArtistRepository mockArtistRepo = mock(ArtistRepository.class);
-        Artist artist = new Artist();
         artist.setCreatedMedia(Collections.emptyList());
-        artistService = new ArtistService(mockArtistRepo);
-        when(mockArtistRepo.findById(anyLong())).thenReturn(Optional.of(artist));
+        when(artistRepositoryMock.findById(anyLong())).thenReturn(Optional.of(artist));
         List<Media> result = artistService.getMediaByArtist(1L);
         assertNotNull(result);
         assertTrue(result.isEmpty());
@@ -45,11 +41,8 @@ class ArtistServiceTest {
 
     @Test
     public void shouldThrowIfNoArtistWithIdCanBeFound(){
-        ArtistRepository mockArtistRepo = mock(ArtistRepository.class);
-        artistService = new ArtistService(mockArtistRepo);
-        when(mockArtistRepo.existsById(anyLong())).thenReturn(false);
+        when(artistRepositoryMock.existsById(anyLong())).thenReturn(false);
         assertThrows(ResourceNotFoundException.class, ()->artistService.getMediaByArtist(1L));
-
     }
 
 
@@ -58,19 +51,18 @@ class ArtistServiceTest {
     public void shouldReturnAllAlbumsWhenArtistExists(){
         long testId = 1L;
         List<Album> albumList = new ArrayList<>(List.of(new Album(), new Album(), new Album()));
-        Artist testArtist = new Artist();
-        testArtist.setAlbums(albumList);
+        artist.setAlbums(albumList);
 
-        when(artistRepositoryMock.findById(testId)).thenReturn(Optional.of(testArtist));
+        when(artistRepositoryMock.findById(testId)).thenReturn(Optional.of(artist));
 
-        assertEquals(albumList, artistService1.getAllAlbums(testId));
+        assertEquals(albumList, artistService.getAllAlbums(testId));
     }
 
     @Test
     public void shouldThrowExceptionWhenNoArtistExists(){
         long testId = 1L;
         when(artistRepositoryMock.findById(testId)).thenReturn(Optional.empty());
-        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, ()-> artistService1.getAllAlbums(testId));
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, ()-> artistService.getAllAlbums(testId));
         assertEquals("Artist not found with id : 1", exception.getMessage());
     }
 
